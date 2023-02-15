d928a1217b34ab540afda16f202a086989b5be97
Richard Lopez <lopezz.richardd@gmail.com>
2023-02-15 23:15:31 +0000

    test

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -10,7 +10,7 @@ public class test {
       @Test
     public void Selenium() throws MalformedURLException {
         ChromeOptions chromeOptions = new ChromeOptions();
-        WebDriver driver = new RemoteWebDriver(new URL("http://137.184.34.196:3333"), chromeOptions);
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
         driver.get("http://www.google.com");
         driver.quit();
         System.out.println("Finalizo");
